f5d50e2f9e19f9c6b0bbf361923b183f020dcb65
Kashyap Sayani <kashyapsayani1@gmail.com>
2023-06-27 23:35:35 +0530

    Removed Warnings

--- src/pages/FeedbackPage.js
+++ src/pages/FeedbackPage.js
@@ -1,4 +1,3 @@
-import React, { useContext, useEffect } from 'react';
 import Navbar from '../components/common/Navbar';
 import FeedbackList from '../components/Feedback/FeedbackList';
 import  { FeedbackProvider } from '../context/FeedbackContext';
